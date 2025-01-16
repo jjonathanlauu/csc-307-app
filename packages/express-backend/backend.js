bcb2646e6d833b8294c5f2be325b3e3dea30ab96
jjonathanlauu <jlau69@calpoly.edu>
2025-01-15 19:21:24 -0800

    backend things

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,8 +3,41 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
+// Users data
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspiring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
 
+// Route to get all users
+app.get("/users", (req, res) => {
+  res.send(users);
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
